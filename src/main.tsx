3ebb416eec9d7dcd7a9059a8050ca14b1f04cf7d
Savindu Amalka <savinduamalka@gmail.com>
2025-11-02 08:26:50 +0530

    feat: update favicon link to use logo from assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
-import { createRoot } from "react-dom/client";
-import App from "./App.tsx";
-import "./index.css";
+import { createRoot } from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import logoUrl from '@/assets/logo.png';
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById('root')!).render(<App />);
+
+const favicon = document.getElementById('favicon') as HTMLLinkElement | null;
+if (favicon) {
+  favicon.href = logoUrl;
+}
